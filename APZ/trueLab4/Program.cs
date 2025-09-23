bfc7a7ab6f9e4342e48837078467f791c462904d
Volodymyr Dudko <dudko.volodimir.dx@gmail.com>
2025-09-23 21:58:28 +0300

    add full lab4

---
+++ APZ/trueLab4/Program.cs
@@ -0,0 +1,23 @@
+﻿using trueLab4.bridge;
+using trueLab4.adapter;
+
+namespace trueLab4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog darkLordCorvaxTheUnmerciful = new Dog("DarkLordCorvaxTheUnmerciful","Chihuahua");
+        Crocodile sunshine = new Crocodile("Sunshine","Crocodile");
+
+        var pond = new Pond();
+        var kennel = new Kennel();
+
+        var settler = new AnimalSettler();
+        
+        Console.WriteLine(settler.SettleAnimal(darkLordCorvaxTheUnmerciful, pond));
+        Console.WriteLine(settler.SettleAnimal(sunshine, kennel));
+        
+        
+    }
+}
